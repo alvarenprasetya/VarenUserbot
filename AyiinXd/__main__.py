4ea88f3bdb3cec2537955949539d83c187668069
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-08-10 09:51:41 +0700

    Update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -39,7 +39,7 @@ try:
     LOGS.info(f"Python Version - {python_version()}")
     LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
     LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
-    LOGS.info(f"NANDEE-Userbot Version - {ubotversion} •[{adB.name}]•")
+    LOGS.info(f"Userbot Version - {ubotversion} •[{adB.name}]•")
     LOGS.info(f"NANDEE Version - {NANDEE_version} •[{HOSTED_ON}]•")
     LOGS.info("[✨ NANDEE-USERBOT BERHASIL DIAKTIFKAN! ✨]")
 except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
